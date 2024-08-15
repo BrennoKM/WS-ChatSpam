9e1908f20f19d43a90863693fe8e6ff42a12f106
Brenno Kevyn <brennokm@gmail.com>
2024-08-15 07:03:42 -0300

    Update spam_post.py

--- spam_post.py
+++ spam_post.py
@@ -8,6 +8,9 @@ import time
 target_process_name = "Warspear Online"
 hwnd = ctypes.windll.user32.FindWindowW(None, target_process_name)
 
+
+# https://learn.microsoft.com/pt-br/windows/win32/inputdev/virtual-key-codes
+
 WM_KEYDOWN = 0x100
 WM_KEYUP = 0x101
 WM_MOUSEMOVE = 0X0200
@@ -56,10 +59,14 @@ VK_W = 0x57 #	W key
 VK_X = 0x58 #	X key
 VK_Y = 0x59 #	Y key
 VK_Z = 0x5A #	Z key
+VK_Ç = 0xBA #   Ç key
 
-VK_OEM_1 = 0xBA # ;:
+VK_OEM_1 = 0xBF # ;:
+VK_OEM_3 = 0xDE # ~^
 VK_SPACE = 0x20 # espaço
 VK_SHIFT = 0x10  # Shift
+VK_OEM_COMMA = 0xBC # ,<
+
 
 def spam(char):
     ctypes.windll.user32.PostMessageW(hwnd, WM_KEYDOWN, char, 0)  # WM_KEYDOWN
@@ -67,9 +74,9 @@ def spam(char):
     ctypes.windll.user32.PostMessageW(hwnd, WM_KEYUP, char, 0)  # WM_KEYUP
     print("apertou")
 
-def spamar():
-    while myEvent.is_set():
-        for char in string:
+def digitar(string):
+    for char in string:
+        if myEvent.is_set():
             char_upper = char.upper()
             if 'A' <= char_upper <= 'Z':
                 vk_code = globals()[f'VK_{char_upper}']
@@ -82,12 +89,27 @@ def spamar():
                 spam_scan(vk_code)
             elif ' ' == char:
                 spam_scan(VK_SPACE)
+            elif ':' == char:
+                spam_scan(VK_OEM_1, True)
+            elif 'ç' == char:
+                spam_scan(VK_Ç) 
+            elif ',' == char:
+                spam_scan(VK_OEM_COMMA)
+            elif 'ã' == char:
+                spam_scan(VK_OEM_3)
+                spam_scan(VK_A)
+            elif 'ô' == char:
+                spam_scan(VK_OEM_3, True)
+                time.sleep(0.09)
+                spam_scan(VK_O)
             else:
                 # Handle other characters as needed
                 pass
             time.sleep(0.02)
+    if myEvent.is_set():
         spam_scan(VK_RETURN)
-        time.sleep(40)
+        time.sleep(2)
+
 
 def spam_scan(scan_code, shift = False):
     lparam_down = (1 << 0) | (scan_code << 16)
@@ -118,11 +140,16 @@ def listener():
         listener.join()
 
 def on_release(key):
+    # try:
+    #     vk_code = key.vk  # Obtém o VK code
+    #     print(f'Tecla pressionada: {key.char}, VK code: {vk_code}')
+    # except AttributeError:
+    #     print(f'Tecla especial pressionada: {key}, VK code: {key.vk}')
     try:
         # if key.char == "k":
         #     spam(string)
         # if key.char == "l":
-        #     print("L")
+        #     move_to(100,100)
         if key == keyboard.Key.alt_gr:
             if not myEvent.is_set():
                 myEvent.set()
@@ -132,27 +159,27 @@ def on_release(key):
             else:
                 myEvent.clear()
                 print("Desligado")
-        # if key == keyboard.Key.ctrl_r:
+        # if key == keyboard.Key.esc:
         #     myEvent.clear()
     except AttributeError:
                 pass
 
+def spamar():
+    while myEvent.is_set():
+        digitar(string1)
+        digitar(string2)
+        digitar(string3)
+        time.sleep(40)
 
-global string
-string = "oiiiiiiii "
-
+global string1, string2, string3
+string1 = "Alô, me chama no zap 85 08130 2220 :flowergrinch::kissing:"
+string2 = "Faço craft Corpo a Corpo, Longo alcance e Tecido :parrot4:"
+string3 = "Mão de obra por 50k com coins e licença incluso :gift:"
+# print(string)
 
 global myEvent
 myEvent = threading.Event()
 myEvent.clear()
-
-# char_a = c_char(b'a')
-# value = ord(char_a.value)
-
-
-# print(f"Valor do caractere 'A': {value}")
-# spam_scan(VK_A)
-# spam(VK_RETURN)
 
 th_l = threading.Thread(target=listener)
 th_l.start()
